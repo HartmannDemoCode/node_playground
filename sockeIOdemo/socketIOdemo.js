1f557551dd4528dd764d2c821d75d0094ca6bb39
Thomas Hartmann <tha@cphbusiness.dk>
2017-09-28 11:50:40 +0200

    expressDemo, socketDemo and readme overview

--- sockeIOdemo/socketIOdemo.js
+++ sockeIOdemo/socketIOdemo.js
@@ -17,13 +17,21 @@ var io = require('socket.io').listen(server);
 
 // When a client connects, we note it in the console
 io.sockets.on('connection', function (socket) {
+    //The server stores Óne socket object for each client.
     socket.emit('message', 'You are connected!');
     console.log('A client is connected!');
 
+    socket.on('sendUserName', function (username) {
+        socket.userName = username;
+        console.log(username + ' has now connected');
+    });
+
     // When the server receives a “message” type signal from the client
     socket.on('message', function (message) {
-        console.log('A client is speaking to me! They’re saying: ' + message);
+        console.log(socket.userName+' is speaking to me! They’re saying: ' + message);
+        socket.broadcast.emit('message', socket.userName+': '+message);
     });
+    socket.broadcast.emit('message', 'A new client just connected');
 });
 
 server.listen(8001);
